573aaf762c2ac1ee7c2bdb79d195368e39cf3fed
Renar Freitas <renarfreitas@gmail.com>
2022-03-03 21:58:03 -0300

    Booleans e Comparações

---
+++ booleanos e comparações - aula 04-02/app.js
@@ -0,0 +1,42 @@
+// Booleans e comparações
+// console.log(true, false, 'true', 'false')
+
+// Métodos podem retornar booleans
+const email = 'brucewayne@rogermelo.com.br'
+const includes = email.includes('@')
+const names = ['dio', 'roger', 'robert']
+const arrayIncludes = names.includes('roger')
+
+// console.log(arrayIncludes)
+
+// Operadores de comparação
+const age = 31
+const name = 'roger'
+
+// console.log(age == 31) // igualdade:
+// console.log(age == 35)
+// console.log(age != 31) // não é igual:
+// console.log(age > 31) // maior quê:
+// console.log(age < 32) // menor quê:
+// console.log(age <= 31) // menor igual a:
+// console.log(age >= 31) // maior ou igual a:
+
+// console.log(name == 'roger')
+// console.log(name == 'Roger')
+// console.log(name > 'belinha')
+// console.log(name > 'Roger') // caracteres minuscos são mairoes que maiusculos.
+// console.log(name > 'Belinha')
+
+// 'igual a' e 'diferente de'
+// console.log(age == 31)
+// console.log(age =='31')
+
+// console.log(age != 31)
+// console.log(age !='31')
+
+// 'igual e, e do mesmo tipo' e 'diferente de, e do mesmo tipo'
+
+// console.log(age === 31)
+// console.log(age === '31')
+console.log(age !== 31)
+console.log(age !== '31')
