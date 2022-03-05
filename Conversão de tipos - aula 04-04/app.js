a915334bf86e5f70d59eef84505151eca99e0244
Renar Freitas <renarfreitas@gmail.com>
2022-03-04 22:48:02 -0300

    Conversão de tipos

---
+++ Conversão de tipos - aula 04-04/app.js
@@ -0,0 +1,27 @@
+// Conversão de tipos
+let score ='100'
+
+// score = Number(score)
+
+// console.log(score + 1)
+// console.log(typeof score)
+
+const crazyConversion = Number('Maçã')
+const converterdNumber = String(97)
+const booleanConversion = Boolean(10)
+
+console.log(Boolean(0, false, "", '', ``, null, undefined, NaN))
+
+/*
+    Valores falsy:
+    - false
+    - 0
+    - "", '', ``
+    - null
+    - undefined
+    - NaN
+
+    Valores Truthy:
+    - Qualquer valor que não seja falsy
+*/
+console.log(crazyConversion, typeof booleanConversion)
