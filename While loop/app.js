4fc304a5e6abc837341f6373e2391b85e9057886
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-03-09 20:00:41 -0300

    Aula sobre Loop com while

---
+++ While loop/app.js
@@ -0,0 +1,16 @@
+// While loop
+
+// let i = 0
+
+// while(i < 5) {
+//     console.log(`Dentro do loop: ${i}`)
+//     i++
+// }
+
+const names = ['Jobs', 'Hopper', 'Turing']
+let i = 0
+
+while ( i < names.length ) {
+    console.log(names[i])
+    i++
+}
