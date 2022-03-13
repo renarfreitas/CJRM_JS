af7d6e74453219114be303f2a2239a1e15b48232
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-03-12 22:51:09 -0300

    Aula sobre condicionais if

---
+++ condicional if - aula 02-02/app.js
@@ -0,0 +1,18 @@
+// if
+// const age = 19
+
+// if (age > 18){
+//     console.log('Você tem mais de 18 anos')
+// }
+
+// const simpsons = ['Marge', 'Homer', 'Lisa', 'Bart']
+
+// if (simpsons.length >= 3) {
+//     console.log('O array tem bastante personagens')
+// }
+
+const password = 'oi123321'
+
+if (password.length >= 8) {
+    console.log('Essa senha tem 8 ou mais caracteres')
+}
