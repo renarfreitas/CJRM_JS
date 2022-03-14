7188562abb062b17c35a4f05194d2c67e4fb13b3
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-03-13 21:58:56 -0300

    Aual else e else if

---
+++ else e else if aula 02-03/app.js
@@ -0,0 +1,10 @@
+// else e else if statements
+const password = 'oi123'
+
+if (password.length >=12) {
+    console.log('senha muito forte =)')
+} else if(password.length >= 8) {
+    console.log('essa senha tem 8 ou mais caracteres')
+} else {
+    console.log('a senha deve contar 8 ou mais caracteres')
+}
