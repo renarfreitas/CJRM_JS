dd72535b07b48d961da2079dcc36b66b0ae4b83e
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-03-15 22:47:39 -0300

    Aula sobre o operdador lógico ! = not.

---
+++ operador logico not (!)/app.js
@@ -0,0 +1,9 @@
+// Operador lógico not (!)
+let isUserLoggedIn = false
+
+if (!isUserLoggedIn) {
+    console.log('Você precisa fazer login para continuar')
+}
+
+console.log(!true)
+console.log(!false)
