5e5371bce604a5fe82740aeeeddc784333fd1021
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-03-17 22:06:31 -0300

    Switch statement

---
+++ a condicional switch/app.js
@@ -0,0 +1,22 @@
+// A condicional switch
+const grade = 'B'
+
+switch (grade) {
+    case 'A':
+        console.log('Você tirou um A')
+        break
+    case 'B':
+        console.log('Você tirou um B')
+        break
+    case 'C':
+        console.log('Você tirou um C')
+        break
+    case 'D':
+        console.log('Você tirou um D')
+        break
+    case 'E':
+        console.log('Você tirou um E')
+        break
+    default:
+        console.log('nota  inválida')
+}
