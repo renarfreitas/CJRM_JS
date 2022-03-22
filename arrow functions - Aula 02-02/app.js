447d0ebad2e5d7b1dec4fc7a891279df7788b85f
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-03-21 22:58:33 -0300

    Arrow Function

---
+++ arrow functions - Aula 02-02/app.js
@@ -0,0 +1,17 @@
+// função tradicional
+// const double = function (number) {
+//     return number * 2
+// }
+
+// arrow function
+// const double = (number) => {
+//     return number * 2
+// }
+
+// simplificando a arrow function 
+const double = number => number * 2 // default parametres não podemm ser usados nesta sintaxe.
+
+
+
+const result = double(3)
+console.log(`O resultado é: ${result}`)
