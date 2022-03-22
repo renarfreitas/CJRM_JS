ec608cf0726addf12d2d251be78d871719d68863
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-03-22 19:45:00 -0300

    Funções e Métodos - Aula 02-03

---
+++ funções vs métodos Aula 02-03/app.js
@@ -0,0 +1,13 @@
+const name = 'Renar'
+
+// funções
+const sayHai = () => 'oi'
+
+const greet = sayHai()
+console.log(greet)
+
+// métodos
+const nameInToUpperCase = name.toUpperCase() // métodos são funções, associadas a objetos ou tipos de dados.
+// métodos usa a notação de ponto.
+
+console.log(nameInToUpperCase)
