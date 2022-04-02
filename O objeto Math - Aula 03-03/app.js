b8d198a9c94f2eaa56001d3c248b750d9de41239
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-04-02 15:18:51 -0300

    Aula 03-03

---
+++ O objeto Math - Aula 03-03/app.js
@@ -0,0 +1,18 @@
+// O objeto Math
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.E)
+
+const area = 7.7
+
+console.log(Math.round(area))
+console.log(Math.floor(area))
+console.log(Math.ceil(area))
+console.log(Math.trunc(area))
+
+// numeros aleatórios
+const randomNumber = Math.random()
+
+console.log(randomNumber)
+
+console.log(Math.round(randomNumber * 100))
