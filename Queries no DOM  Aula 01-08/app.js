befca5915706b1c2ffc1905ff748ec36d1c837a5
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-04-04 20:14:55 -0300

    Outras maneiras de fazer queries no DOM Aula 01-08

---
+++ Queries no DOM  Aula 01-08/app.js
@@ -0,0 +1,14 @@
+// obter um elemento através do ID
+const title = document.getElementById('title')
+
+console.log(title)
+
+// obter elementos atrvés do nome da classa
+const erros = document.getElementsByClassName('error')
+
+console.log(erros[1])
+
+// obter elementos atrvés do nome da tag
+const paragraphs = document.getElementsByTagName('p')
+
+console.log(paragraphs)
