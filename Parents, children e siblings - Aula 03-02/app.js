046421ec037cfa52221a660f3a955084895a8802
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-04-12 20:03:26 -0300

    Parents, children e siblings - Aula 03-02

---
+++ Parents, children e siblings - Aula 03-02/app.js
@@ -0,0 +1,13 @@
+const article = document.querySelector('article')
+
+// Array.from(article.children).forEach( element => {
+//   element.classList.add('article-element')
+// })
+
+const title = document.querySelector('h2')
+
+// console.log(title.parentElement.parentElement)
+
+// console.log(title.nextElementSibling)
+
+console.log(title.previousElementSibling)
