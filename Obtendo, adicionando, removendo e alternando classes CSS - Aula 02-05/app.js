9bad0d6bb93e94a0ac0a2cd7122332ec3f2d4017
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-04-11 21:01:20 -0300

    Obte. adicion. remove. alter. classes CSSAula 02-05

---
+++ Obtendo, adicionando, removendo e alternando classes CSS - Aula 02-05/app.js
@@ -0,0 +1,20 @@
+// const paragraphs = document.querySelector('p')
+
+// console.log(paragraph.classList)
+
+// paragraph.classList.add('error')
+// paragraph.classList.remove('error')
+// paragraph.classList.add('success')
+const paragraphs = document.querySelectorAll('p')
+const title = document.querySelector('h1')
+
+paragraphs.forEach( paragraph => {
+  if(paragraph.textContent.includes('success')){
+    paragraph.classList.add('success')
+  } else if (paragraph.textContent.includes('error')) {
+    paragraph.classList.add('error')
+  }
+})
+
+title.classList.toggle('test')
+title.classList.toggle('test')
