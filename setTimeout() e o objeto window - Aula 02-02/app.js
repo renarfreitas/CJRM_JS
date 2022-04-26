f3cdd94793ae51dc75e4191ce85633ebe1e03362
Renar Freitas <38799859+renarfreitas@users.noreply.github.com>
2022-04-25 22:40:07 -0300

    setTimeout() e o objeto window - Aula 02-02

---
+++ setTimeout() e o objeto window - Aula 02-02/app.js
@@ -0,0 +1,10 @@
+/* 
+O objeto window 
+Tudo que é feito no JavaScript, no front-end, todos os métdos e propriedades estão armazenados no objeto window.
+*/
+
+setTimeout(()=>{
+  alert('executou')
+}, 2000) // Executa uma função apos quantidade de tempo estipulada.
+
+scrollTo(0, 0) // está função recebe dois argumentos, as coordenadas x e y, do que deve ser exibido no canto superiro esquedo da tela.
