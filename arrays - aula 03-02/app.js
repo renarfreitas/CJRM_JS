9cd1b5bef5986a390702cf9e9d3202a155eed68f
Renar Freitas <renarfreitas@gmail.com>
2024-01-17 21:47:18 -0300

    Arrays e métodos

--- arrays - aula 03-02/app.js
+++ arrays - aula 03-02/app.js
@@ -3,20 +3,21 @@ let heroes = ['Batman', 'Catwoman', 'Iron Man']
 
 heroes[2] = 'Spider-Man'
 
+console.log(heroes)
 const ages = [31, 25, 39, 40, 25]
 
 const randomArray = ['Parker', 'Diana', 19, 18]
 
-
 // Métodos de arrays
 console.log(heroes.length)
 
-const joinHeroes = heroes.join('-')
-const indexOf25 = ages.indexOf('25')
+const joinHeroes = heroes.join(' - ')
+console.log(joinHeroes)
+const indexOf25 = ages.indexOf(25)
+console.log(indexOf25)
 const moreHeroes = heroes.concat(['Superman', 'Wolverine'])
+console.log(moreHeroes)
 const pushToHeroes = heroes.push('Cyclops', 'Hulk') // Mutação de valores
-const popHeroes = heroes.pop()
+const popHeroes = heroes.pop() // Mutação de valores
 
 console.log(moreHeroes, heroes, popHeroes)
-
-
