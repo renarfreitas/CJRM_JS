23beecfb42a4081a8ed86b9703bbd5a9bc5390f0
Renar Freitas <renarfreitas@gmail.com>
2024-01-18 22:14:07 -0300

    revisão de null e undefined

--- null-undefined - aula 03-03/app.js
+++ null-undefined - aula 03-03/app.js
@@ -1,3 +1,7 @@
 let emptiness = null // indica intencionalmente que não existe valor em uma variável.
 
 console.log(emptiness, emptiness + 3, `o valor é ${emptiness}`)
+
+let undef // undefined é atriuido automaticamente a está variávels
+
+console.log(undef)
