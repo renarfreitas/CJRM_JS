017030ae3c39b4ba987e5495b99dce8a25c11e8a
Renar Freitas <renarfreitas@gmail.com>
2024-01-22 21:58:04 -0300

    Revisão de operadores

--- booleanos e comparações - aula 04-02/app.js
+++ booleanos e comparações - aula 04-02/app.js
@@ -1,5 +1,5 @@
 // Booleans e comparações
-// console.log(true, false, 'true', 'false')
+console.log(true, false, 'true', 'false')
 
 // Métodos podem retornar booleans
 const email = 'brucewayne@rogermelo.com.br'
@@ -7,36 +7,36 @@ const includes = email.includes('@')
 const names = ['dio', 'roger', 'robert']
 const arrayIncludes = names.includes('roger')
 
-// console.log(arrayIncludes)
+console.log(arrayIncludes)
 
 // Operadores de comparação
 const age = 31
 const name = 'roger'
 
-// console.log(age == 31) // igualdade:
-// console.log(age == 35)
-// console.log(age != 31) // não é igual:
-// console.log(age > 31) // maior quê:
-// console.log(age < 32) // menor quê:
-// console.log(age <= 31) // menor igual a:
-// console.log(age >= 31) // maior ou igual a:
+console.log(age == 31) // igualdade:
+console.log(age == 35)
+console.log(age != 31) // não é igual:
+console.log(age > 31) // maior quê:
+console.log(age < 32) // menor quê:
+console.log(age <= 31) // menor igual a:
+console.log(age >= 31) // maior ou igual a:
 
-// console.log(name == 'roger')
-// console.log(name == 'Roger')
-// console.log(name > 'belinha')
-// console.log(name > 'Roger') // caracteres minuscos são mairoes que maiusculos.
-// console.log(name > 'Belinha')
+console.log(name == 'roger')
+console.log(name == 'Roger')
+console.log(name > 'belinha')
+console.log(name > 'Roger') // caracteres minuscos são mairoes que maiusculos.
+console.log(name > 'Belinha')
 
 // 'igual a' e 'diferente de'
-// console.log(age == 31)
-// console.log(age =='31')
+console.log(age == 31)
+console.log(age == '31')
 
-// console.log(age != 31)
-// console.log(age !='31')
+console.log(age != 31)
+console.log(age != '31')
 
 // 'igual e, e do mesmo tipo' e 'diferente de, e do mesmo tipo'
 
-// console.log(age === 31)
-// console.log(age === '31')
+console.log(age === 31)
+console.log(age === '31')
 console.log(age !== 31)
 console.log(age !== '31')
