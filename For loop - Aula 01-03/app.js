f6d905416059a8b6fad6b1b12023b1e64dd47e51
Renar Freitas <renarfreitas@gmail.com>
2024-01-25 22:10:12 -0300

    Revisão for loop

--- For loop - Aula 01-03/app.js
+++ For loop - Aula 01-03/app.js
@@ -1,12 +1,18 @@
 // for loop
-// for (let i = 0; i < 5; i++) {
-//     console.log(`Dentro do loop: ${i}`)
-// }
-// console.log('Loop concluído')
+for (let i = 0; i < 5; i++) {
+  console.log(`Dentro do loop: ${i}`)
+}
+console.log('Loop concluído')
 
 const names = ['Linus', 'Ada', 'Bill']
+const hh1 = document.querySelector('h1')
 
 for (let i = 0; i < names.length; i++) {
-    const HTMLTemplate = `<p>${names[i]}</p>`
-    console.log(HTMLTemplate)
+  const HTMLTemplate = `<p>${names[i]}</p>`
+  console.log(HTMLTemplate)
+}
+
+let t
+for (t = 0; t < 5; t++) {
+  console.log(`voccê é um cabeção!${t}`)
 }
