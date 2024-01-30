6b4a44e3069287e88a4c77fb6a2964a935743967
Renar Freitas <renarfreitas@gmail.com>
2024-01-30 19:44:53 -0300

    Revisão if

--- condicional if - aula 02-02/app.js
+++ condicional if - aula 02-02/app.js
@@ -1,18 +1,18 @@
 // if
-// const age = 19
+const age = 16
 
-// if (age > 18){
-//     console.log('Você tem mais de 18 anos')
-// }
+if (age > 18) {
+  console.log('Você tem mais de 18 anos')
+}
 
-// const simpsons = ['Marge', 'Homer', 'Lisa', 'Bart']
+const simpsons = ['Marge', 'Homer', 'Lisa', 'Bart']
 
-// if (simpsons.length >= 3) {
-//     console.log('O array tem bastante personagens')
-// }
+if (simpsons.length >= 3) {
+  console.log('O array tem bastante personagens')
+}
 
 const password = 'oi123321'
 
 if (password.length >= 8) {
-    console.log('Essa senha tem 8 ou mais caracteres')
+  console.log('Essa senha tem 8 ou mais caracteres')
 }
