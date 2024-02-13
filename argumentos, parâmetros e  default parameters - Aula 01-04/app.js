bfb7a36f4affedb273bede2540a9c75ed5173511
Renar Freitas <renarfreitas@gmail.com>
2024-02-13 20:54:41 -0300

    Revisão Funções Parametros e Arguentos

--- argumentos, parâmetros e  default parameters - Aula 01-04/app.js
+++ argumentos, parâmetros e  default parameters - Aula 01-04/app.js
@@ -1,5 +1,13 @@
 // Argumentos, parâmetros e default parameters
-const myFunc = function (name = 'Estrôncio', lastName = 'Almeida') {
-    console.log(`Oi, ${name} ${lastName}!`)
+
+const myFunc2 = function () {
+  console.log('Oi, Fulano!')
 }
+
+const myFunc = function (name = 'Estrôncio', lastName = 'Almeida') {
+  console.log(`Oi, ${name} ${lastName}!`)
+}
+
+myFunc2()
+
 myFunc('Renar', 'Freitas')
