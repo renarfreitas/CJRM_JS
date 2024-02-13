a8a3a7589f103b417d712675efdaea4b74009702
Renar Freitas <renarfreitas@gmail.com>
2024-02-12 21:31:32 -0300

    Revisando funções

--- funções - Aula 01-02 e 03/app.js
+++ funções - Aula 01-02 e 03/app.js
@@ -1,11 +1,12 @@
 // Function declaration * Aqui acontece o hoisting: por baixo dos "panos" o javascript pucha a declaração da função para o topo do arquivo.
-function sayHi () {
-    console.log('oi')
+
+function sayHi() {
+  console.log('oi')
 }
 
 // Function expression * Aqui o hoisting não funciona.
 const showFood = function () {
-    console.log('pizza')
+  console.log('pizza')
 }
 
 sayHi()
