d73fc615d5fe2b322535e0dcfaf41f0653b191a5
Renar Freitas <renarfreitas@gmail.com>
2024-02-20 22:17:27 -0300

    Revisão de forEach e calbacks

--- o método forEach e callbacks/app.js
+++ o método forEach e callbacks/app.js
@@ -1,18 +1,18 @@
-// forEach e callbacks são funções passadas como argumentos da  invocaçaõ de outras funções ou de um método.
+// forEach e callbacks são funções passadas como argumentos da  invocação de outras funções ou de um método.
 const myFunc = callback => {
-    const value = 77
+  const value = 77
 
-    callback(value)
+  callback(value)
 }
 
 myFunc(number => {
-    console.log(number)
+  console.log(number)
 })
 
 const socialNetworks = ['youtube', 'twitter', 'instagram', 'facebook']
 
 const logArrayInfo = (socialNetwork, index, array) => {
-    console.log(index, socialNetwork, array)
+  console.log(index, socialNetwork, array)
 }
 
 socialNetworks.forEach(logArrayInfo)
