0fa70a87939f9cb1954d72f7ecdefa5a8d6c2a78
Renar Freitas <renarfreitas@gmail.com>
2024-02-22 21:51:34 -0300

    Exercício de Revisão

--- exercicio-09/app.js
+++ exercicio-09/app.js
@@ -19,7 +19,7 @@
 
 const convertToString = value => String(value)
 
-console.log(convertToString(10))
+console.log(typeof convertToString(10))
 /*
   02
 
@@ -40,7 +40,11 @@ console.log(stringLength(convertToString(10)))
 */
 const convertToLowerCase = string => string.toLowerCase()
 
-console.log(convertToLowerCase('CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO'))
+console.log(
+  convertToLowerCase(
+    'CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO'
+  )
+)
 /*
   04
 
@@ -58,7 +62,7 @@ console.log(indexOfString('a', 'casa'))
 */
 const booleanRetorn = (item, array) => array.includes(item)
 
-console.log(booleanRetorn(1, [2,5,3,1]))
+console.log(booleanRetorn(1, [2, 5, 3, 1]))
 /*
   06
 
@@ -67,7 +71,7 @@ console.log(booleanRetorn(1, [2,5,3,1]))
 */
 const concatArray = (array, array1) => array.concat(array1)
 
-console.log(concatArray([1,2,3], [6,5,4]))
+console.log(concatArray([1, 2, 3], [6, 5, 4]))
 /*
   07
 
@@ -79,7 +83,7 @@ const popArray = array => {
   return array
 }
 
-console.log(popArray([1,2,3,4,5]))
+console.log(popArray([1, 2, 3, 4, 5]))
 /*
   08
 
@@ -152,7 +156,7 @@ numbers.forEach(showNumbersInfo)
 const letters = ['v', 'e', 'p']
 let lettersCopy = []
 
-letters.forEach((vowel)=>{
+letters.forEach(vowel => {
   lettersCopy.push(vowel)
 })
 
@@ -182,7 +186,7 @@ console.log(lettersCopy)
 const section = document.querySelector('[data-js="section"]')
 
 const review = [
-  'Eu sempre adorei o filme e quando descobri que tinha o livro também fiquei doido. Demorei um pouco mas acabei comprando e finalmente li \o/.',
+  'Eu sempre adorei o filme e quando descobri que tinha o livro também fiquei doido. Demorei um pouco mas acabei comprando e finalmente li o/.',
   'O primeiro filme foi baseado nesse livro, porém o livro (como sempre) é muito mais completo, com mais personagens, mais acontecimentos e até mesmo mais dinossauros. Na verdade nesse livro tem coisas do segundo e terceiro filme também, eles mudaram bastante nos filmes, acho que pra ficar mais comercial, e se o filme é bom, o livro é 100 vezes melhor.',
   'Michael é um ótimo autor, esse sim pesquisa muito antes de escrever um livro, além da história que já prende sua atenção, ele fala bastante de genética (pra explicar como os dinossauros foram criados) e acaba falando um pouco de programação (informática), por causa dos programas avançados e modernos que o parque tinha. E isso foi uma das coisas que eu achei muito legal, ele explica as coisas com gráficos, tabelas, códigos ... enfim, o cara é foda hahaha.',
   'Recomendo esse livro pra quem curte uma boa história de ficção. Apesar de muita gente pensar que o livro não tem graça, porque o legal mesmo é ver o dinossauro no filme, com todos os efeitos especiais, eu digo pra deixar esse pensamento de lado, pois a história é tão bem contada e os detalhes são tão bem relatados, que você passa a fazer parte da história, e vive todas as emoções hahaha.'
@@ -190,7 +194,7 @@ const review = [
 let paragraphs = ''
 
 const createParagraph = paragraph => {
-  paragraphs += `<p>${paragraph}</p>`  
+  paragraphs += `<p>${paragraph}</p>`
 }
 
 review.forEach(createParagraph)
@@ -226,7 +230,7 @@ const peopleAmout = array => {
   const thirdName = array[3]
   const totalNamesMinusTwo = array.length - 2
 
-  switch(array.length) {
+  switch (array.length) {
     case 0:
       return 'Ninguém curtiu isso'
     case 1:
@@ -237,7 +241,7 @@ const peopleAmout = array => {
       return `${firstName}, ${secondeName} e ${thirdName} curtiram isso`
     default:
       return `${firstName}, ${secondeName} e mais ${totalNamesMinusTwo} pessoas curtiram isso`
-    }
+  }
 }
 
 console.log(peopleAmout(peoples))
