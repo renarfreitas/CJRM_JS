80e440c6d94344f151416793ec5fad7b4c85eea7
Renar Freitas <renarfreitas@gmail.com>
2024-02-23 21:57:45 -0300

    Revisando a criação de objetos

--- objetos - Aula 01-02/app.js
+++ objetos - Aula 01-02/app.js
@@ -1,20 +1,34 @@
 // Objetos literal
 let user = {
-    name:'Renar',
-    age: 39,
-    email: 'renarfreitas@gmail.com',
-    city: 'Salvador',
-    blogPosts:  ['Empadão de frango', '4 receitas de pure de batatas']
+  name: 'Renar',
+  age: 39,
+  email: 'renarfreitas@gmail.com',
+  city: 'Salvador',
+  blogPosts: ['Feijoada', 'Banofe de Banana']
 }
 
+console.log(user)
 console.log(user.name)
 // sobreescrevendo propriedades
 
 user.age = 32
+
+console.log(user.name)
 console.log(user.age)
+console.log(user.email)
+console.log(user.city)
+console.log(user.blogPosts)
 
 // acessando propriedade com notação de colchetes
 
 console.log(user['email'])
-user['name'] = 'Freitas'
+
+const key = 'email'
+
+console.log(user[key])
+// Atribuir valores com notação de cochete
+// user['name'] = 'Freitas'
+
 console.log(user['name'])
+
+console.log(typeof user)
