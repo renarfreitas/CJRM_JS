21895aeccc309588631257f4db01b3ba9d72a2df
Renar Freitas <renarfreitas@gmail.com>
2024-03-04 22:02:06 -0300

    Revisão Variáveis e escopo de bloco - Aula 02-02

--- variáveis e escopo de bloco/app.js
+++ variáveis e escopo de bloco/app.js
@@ -2,20 +2,18 @@
 
 const age = 31 // escopo global
 
-
-
 if (true) {
-    const age = 41 // escopo local de bloco
-    const name = 'Renar'
+  const age = 42 // escopo local de bloco
+  const name = 'Renar'
 
-    console.log(`Dentro do 1º bloco de código: ${age}, ${name}`)
+  console.log(`Dentro do 1º bloco de código: ${age}, ${name}`)
 
-    if (true) {
-        const age = 51
-        console.log(`Dentro do 2º bloco de código: ${age}`)
+  if (true) {
+    const age = 41
+    console.log(`Dentro do 2º bloco de código: ${age}`)
 
-        var test = 'oi' // foge do escopo.
-    }
+    var test = 'oi' // foge do escopo.
+  }
 }
 
 console.log(`Fora do bloco de  código: ${age} ${name} ${test}`)
