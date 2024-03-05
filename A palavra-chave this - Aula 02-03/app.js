e00cacd024bfdf2161790faf33473a057a5625ea
Renar Freitas <renarfreitas@gmail.com>
2024-03-05 05:31:09 -0300

    Revisão da A palavra-chave this - Aula 02-03

--- A palavra-chave this - Aula 02-03/app.js
+++ A palavra-chave this - Aula 02-03/app.js
@@ -1,23 +1,24 @@
 // A palavra-chave this
 
 let user = {
-    name: 'João',
-    age:31,
-    email:'joaocardoso@gmail.com',
-    city:'São Paulo', 
-    blogPosts: ['Empadão de frango', '4 receitas de purê de batata'],
-    login: function () {
-        console.log('Usuário logado')
-    },
-    logout: function () {
-        console.log('Usuário deslogado') // function declaration 
-    },
-    logBlogPosots () { // shortcut para function declaration.
-        console.log(`${this.name} escreveu os seguintes posts:`)
-        this.blogPosts.forEach( post => {
-            console.log(post)
-        }) // Objeto de contexto // não usar o arrow fuction ficar atento ao contexto
-    }
+  name: 'Renar',
+  age: 31,
+  email: 'renarfreitas@gmail.com',
+  city: 'São Paulo',
+  blogPosts: ['Banofe de Banana', '4 receitas de purê de batata'],
+  login: function () {
+    console.log('Usuário logado')
+  },
+  logout: function () {
+    console.log('Usuário deslogado') // function declaration
+  },
+  logBlogPosots() {
+    // shortcut para function declaration sintax reduzida ': function', forma  removidos
+    console.log(`${this.name} escreveu os seguintes posts:`)
+    this.blogPosts.forEach(post => {
+      console.log(post)
+    }) // Objeto de contexto // não usar o arrow fuction ficar atento ao contexto
+  }
 }
 user.login()
 user.logout()
