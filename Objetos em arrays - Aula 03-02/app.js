95e4b625d1548ac8859023034c26adb15c43df4c
Renar Freitas <renarfreitas@gmail.com>
2024-03-07 05:13:43 -0300

    Revisão da aula:  Objetos em Array - Aula 03-02

--- Objetos em arrays - Aula 03-02/app.js
+++ Objetos em arrays - Aula 03-02/app.js
@@ -1,25 +1,26 @@
 // Objetos em arrays
 let user = {
-    name: 'João',
-    age: 31,
-    email: 'joaocardoso@gmail.com',
-    city: 'São Paulo', 
-    blogPosts: [
-        {title: 'Empadão de frango', likes: 30},
-        {title: '4 receitas de purê de batata', likes: 50}
-    ],
-    login () {
-        console.log('Usuário logado')
-    },
-    logout () {
-        console.log('Usuário deslogado') // function declaration 
-    },
-    logBlogPosots () { // shortcut para function declaration.
-        console.log(`${this.name} escreveu os seguintes posts:`)
-        this.blogPosts.forEach( post => {
-            console.log(`O Post: ${post.title}, teve: ${post.likes}`)
-        }) // Objeto de contexto // não usar o arrow fuction ficar atento ao contexto
-    }
+  name: 'Renar',
+  age: 41,
+  email: 'renarfreitas@gmail.com',
+  city: 'Salvador',
+  blogPosts: [
+    { title: 'Bamofe de banana', likes: 3 },
+    { title: '4 receitas de purê de batata', likes: 50 }
+  ],
+  login() {
+    console.log('Usuário logado')
+  },
+  logout() {
+    console.log('Usuário deslogado') // function declaration
+  },
+  logBlogPosots() {
+    // shortcut para function declaration.
+    console.log(`${this.name} escreveu os seguintes posts:`)
+    this.blogPosts.forEach(post => {
+      console.log(`O Post: ${post.title}, teve: ${post.likes} likes`)
+    }) // Objeto de contexto // não usar o arrow fuction ficar atento ao contexto
+  }
 }
 
 user.logBlogPosots()
