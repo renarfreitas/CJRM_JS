99da3401cf227c91ae5e7d7e8863255b789986a7
Renar Freitas <renarfreitas@gmail.com>
2024-03-08 08:18:05 -0300

    Revsão da aula  - O objeto Math - Aula 03-03

--- O objeto Math - Aula 03-03/app.js
+++ O objeto Math - Aula 03-03/app.js
@@ -5,14 +5,14 @@ console.log(Math.E)
 
 const area = 7.7
 
-console.log(Math.round(area))
-console.log(Math.floor(area))
-console.log(Math.ceil(area))
-console.log(Math.trunc(area))
+console.log(Math.round(area)) //Converte um número decimal de forma padrão >.5 para cima <.5 para baixo
+console.log(Math.floor(area)) //Converte um número decimal sempre para cima
+console.log(Math.ceil(area)) //Converte um número decimal sempre para baixo
+console.log(Math.trunc(area)) //Remove o decimal de um número
 
 // numeros aleatórios
 const randomNumber = Math.random()
 
 console.log(randomNumber)
 
-console.log(Math.round(randomNumber * 100))
+console.log(Math.round(randomNumber * 100)) // Gera um número aletorio entre 0 e 100
