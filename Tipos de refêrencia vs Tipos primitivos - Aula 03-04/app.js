fa488880e2d3f4865bc52ba088af7006155e26d0
Renar Freitas <renarfreitas@gmail.com>
2024-03-11 22:25:12 -0300

    Revisão: Tipos primitivos - Aula 03-04

--- Tipos de refêrencia vs Tipos primitivos - Aula 03-04/app.js
+++ Tipos de refêrencia vs Tipos primitivos - Aula 03-04/app.js
@@ -1,22 +1,27 @@
+// Valores Primitivos
+
 // Tipos de refêrencia vs Tipos primitivos
 
+/* stack  =>  heap 
+Stack = tradução livre, Pilha;
+Heap = Tradução livre, amontoado.
+*/
 
-/* stack  =>  heap */
+let scoreOne = 50
+let scoreTwo = scoreOne
 
-// let scoreOne = 50
-// let scoreTwo = scoreOne
+console.log(`socreONe: ${scoreOne} | socreTwo: ${scoreTwo}`)
 
-// console.log(`socreONe: ${scoreOne} | socreTwo: ${scoreTwo}`)
+scoreOne = 100
 
-// scoreOne = 100
+console.log(`socreONe: ${scoreOne} | socreTwo: ${scoreTwo}`)
 
-// console.log(`socreONe: ${scoreOne} | socreTwo: ${scoreTwo}`)
-
-let userOne ={name:'Renar', age: 31}
+// Valores de referência
+let userOne = { name: 'Renar', age: 41 }
 let userTwo = userOne
 
 console.log(userOne, userTwo)
 
-userOne.age = 32
+userOne.age = 42
 
 console.log(userOne, userTwo)
