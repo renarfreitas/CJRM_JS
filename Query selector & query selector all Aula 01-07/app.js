faf021146acfbd9c7838dee141b955d713b72b9a
Renar Freitas <renarfreitas@gmail.com>
2024-03-18 05:14:09 -0300

    Revisão query selector all

--- Query selector & query selector all Aula 01-07/app.js
+++ Query selector & query selector all Aula 01-07/app.js
@@ -1,21 +1,21 @@
-// //Query selector & query selector all
-// const paragraph = document.querySelector('.error')
-// const errorDiv = document.querySelector('div.error')
-// const h1 = document.querySelector('body > h1')
-// const paragraphs = document.querySelectorAll('p')
+//Query selector & query selector all
+const paragraph = document.querySelector('.error')
+const errorDiv = document.querySelector('div.error')
+const h1 = document.querySelector('body > h1')
+const paragraphs = document.querySelectorAll('p')
 
-// // console.log(paragraphs[0])
+// console.log(paragraphs[0])
 
-// paragraphs.forEach(element => {
-//     console.log(element)
-// })
+paragraphs.forEach(element => {
+  console.log(element)
+})
 
-// // console.log(paragraph, errorDiv, h1)
+// console.log(paragraph, errorDiv, h1)
 
-// const errors = document.querySelectorAll('.error')
+const errors = document.querySelectorAll('.error')
 
-// console.log(errors)
+console.log(errors)
 
-// errors.forEach(lements => {
-//     console.log(lements)
-// })
+errors.forEach(lements => {
+  console.log(lements)
+})
