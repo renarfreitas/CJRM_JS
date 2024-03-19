29f12e8f1206a35481917178eb7e83389542b3c4
Renar Freitas <renarfreitas@gmail.com>
2024-03-19 05:24:16 -0300

    Outras maneiras de fazer queries no DOM - Aula 01-08

--- Queries no DOM  Aula 01-08/app.js
+++ Queries no DOM  Aula 01-08/app.js
@@ -6,7 +6,7 @@ console.log(title)
 // obter elementos atrvés do nome da classa
 const erros = document.getElementsByClassName('error')
 
-console.log(erros[1])
+console.log(erros[1]) // HTMLCollection[], rrepresentação de uma lisat genêrica de elementos. Similar ao nodelist
 
 // obter elementos atrvés do nome da tag
 const paragraphs = document.getElementsByTagName('p')
