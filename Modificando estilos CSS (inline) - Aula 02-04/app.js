eff2c1feaae0c63217988763b691982f923364bc
Renar Freitas <renarfreitas@gmail.com>
2024-03-26 19:36:16 -0300

    Modificando estilos CSS com JS

--- Modificando estilos CSS (inline) - Aula 02-04/app.js
+++ Modificando estilos CSS (inline) - Aula 02-04/app.js
@@ -8,4 +8,8 @@ console.log(title.style.color)
 title.style.margin = '50px'
 title.style.color = 'pink'
 title.style.fontSize = '50px'
-title.style.margin = ''
+// title.style.margin = ''
+
+if (title.textContent.includes('(inline)')) {
+  title.textContent = title.textContent.replace('(inline)', '(in JavaScript)')
+}
