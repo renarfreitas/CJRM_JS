a7f397e5070342ac13c9bacdb7a9e8338db505a3
Renar Freitas <renarfreitas@gmail.com>
2024-03-27 05:27:14 -0300

    Revisão - Aula 02-05
    Obtendo, adicionando, removendo class do css na pagina

--- Obtendo, adicionando, removendo e alternando classes CSS - Aula 02-05/app.js
+++ Obtendo, adicionando, removendo e alternando classes CSS - Aula 02-05/app.js
@@ -8,13 +8,13 @@
 const paragraphs = document.querySelectorAll('p')
 const title = document.querySelector('h1')
 
-paragraphs.forEach( paragraph => {
-  if(paragraph.textContent.includes('success')){
+paragraphs.forEach(paragraph => {
+  if (paragraph.textContent.includes('success')) {
     paragraph.classList.add('success')
   } else if (paragraph.textContent.includes('error')) {
     paragraph.classList.add('error')
   }
 })
 
-title.classList.toggle('test')
-title.classList.toggle('test')
+title.classList.toggle('test') // adiciona a class ao elemento
+title.classList.toggle('test') // remove a class do elelmento caso ele já tenha.
