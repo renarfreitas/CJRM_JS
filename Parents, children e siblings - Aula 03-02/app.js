8e928596bd733fb78b3e94240406f2277dc37efe
Renar Freitas <renarfreitas@gmail.com>
2024-04-09 05:19:56 -0300

    Revisão
    Parents, children e siblings - Aula 03-02

--- Parents, children e siblings - Aula 03-02/app.js
+++ Parents, children e siblings - Aula 03-02/app.js
@@ -1,8 +1,9 @@
 const article = document.querySelector('article')
+console.log(article.children)
 
-// Array.from(article.children).forEach( element => {
-//   element.classList.add('article-element')
-// })
+Array.from(article.children).forEach(element => {
+  element.classList.add('article-element')
+})
 
 const title = document.querySelector('h2')
 
