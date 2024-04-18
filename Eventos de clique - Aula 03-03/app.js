0ea24a342380e79d1d7411e69079a105ab6cb303
Renar Freitas <renarfreitas@gmail.com>
2024-04-17 21:40:38 -0300

    Revisão
    addEventListener

--- Eventos de clique - Aula 03-03/app.js
+++ Eventos de clique - Aula 03-03/app.js
@@ -1,12 +1,13 @@
+// Eventos de click
 const button = document.querySelector('button')
 
 button.addEventListener('click', event => {
   console.log('Clicou no botão')
 })
 
-const lis = document.querySelectorAll('li')
+const lis = document.querySelectorAll('li') // Obter todas as li's da página
 
-lis.forEach( li => {
+lis.forEach(li => {
   li.addEventListener('click', event => {
     const clickedElement = event.target
 
