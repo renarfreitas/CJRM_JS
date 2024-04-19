241f3d3c23eddacba627eafbe6dab741926a417b
Renar Freitas <renarfreitas@gmail.com>
2024-04-19 05:23:30 -0300

    Revisão
    Evente bubbling

--- Event bubbling e event delegation - Aula 04-02/app.js
+++ Event bubbling e event delegation - Aula 04-02/app.js
@@ -5,16 +5,15 @@ const input = document.querySelector('input')
 button.addEventListener('click', event => {
   const li = document.createElement('li')
 
-  li.textContent = 'Novo item'
+  li.textContent = prompt()
   ul.append(li) // ultimo filho do elemento
-  // ul.prepend(li) // primeiro filho do elemento 
-})  
-
+  // ul.prepend(li) // primeiro filho do elemento
+})
 
 ul.addEventListener('click', event => {
   const clickedElement = event.target
 
-  if(clickedElement.tagName === 'LI') {
+  if (clickedElement.tagName === 'LI') {
     clickedElement.remove()
   }
 })
