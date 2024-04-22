bbfe2a7e5d7b07792e558df3b90a6ec6cd48af10
Renar Freitas <renarfreitas@gmail.com>
2024-04-22 20:23:08 -0300

    Revisão
    Mousemove e wheel

--- Mais eventos do DOM - Aula 04-03/app.js
+++ Mais eventos do DOM - Aula 04-03/app.js
@@ -1,15 +1,18 @@
 const paragraph = document.querySelector('.copy-me')
 
-paragraph.addEventListener('copy', () => { // evento de cópia
+paragraph.addEventListener('copy', () => {
+  // evento de cópia
   console.log('Texto copiado!')
 })
 
 const div = document.querySelector('.box')
 
-div.addEventListener('mousemove', event => { // evento de movimentação de mouse
-  div.textContent = `X ${event.offsetX} | Y ${event.offsetY }`
+div.addEventListener('mousemove', event => {
+  // evento de movimentação de mouse
+  div.textContent = `X ${event.offsetX} | Y ${event.offsetY}`
 })
 
-document.addEventListener('wheel', event => { // evento de giro do scroll do mouse 
+document.addEventListener('wheel', event => {
+  // evento de giro do scroll do mouse
   console.log(event.pageX, event.pageY)
 })
