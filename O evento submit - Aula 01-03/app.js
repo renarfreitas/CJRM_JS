582f78aef7b6f7b7375288081fec4aa1424848cd
Renar Freitas <renarfreitas@gmail.com>
2024-05-06 21:38:21 -0300

    Update about Froms submit

--- O evento submit - Aula 01-03/app.js
+++ O evento submit - Aula 01-03/app.js
@@ -1,4 +1,5 @@
-const form = document.querySelector('.signup-form')
+// Eventos de submit
+const form = document.querySelector('.signup-form') // Obtendo e armazenando a referência do form
 const userNameInput = document.querySelector('#username')
 
 form.addEventListener('submit', event => {
@@ -6,4 +7,4 @@ form.addEventListener('submit', event => {
   usuario = event.target.username.value
   alert(`Seu username é: ${usuario}`)
   console.log(event.target.username.value)
-})
+}) // Listener de evento, que fica observardo quando o form é enviado.
