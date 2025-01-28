c8181626a2b4bb5f5bd3b30b9546ec8efe2b4a5f
Renar Freitas <renarfreitas@gmail.com>
2025-01-27 22:02:27 -0300

    Revisão Aula date-Fns
    Revisão a respeito da Lib/Biblioteca sobre a date-Fns.

--- date-fns - Aula 02-03/app.js
+++ date-fns - Aula 02-03/app.js
@@ -1,6 +1,19 @@
-const present = new Date('june 7 2020 15:47:07')
+/*
+date-Fns: É uma lib ou biblioteca para trabalhar com datas em JavaScript,
+para trabalhos com aplicações de médio e grande porte. 
+Pode ser utilizada tanto no broser quando no NODEJS.
+url: https://data-fns.org
+*/
 
-// console.log(dateFns.isToday(present))
+console.log(dateFns)
+const present = new Date('june 7 2020 15:47:07')
+const yesIsToday = new Date()
+
+const result = dateFns.isAfter(present, new Date())
+
+console.log(result)
+console.log(dateFns.format(present, 'MMMM'))
+console.log(dateFns.isToday(yesIsToday)) // função que verifica se a data passada corresponde a data atual
 console.log(dateFns.format(present, 'DD/MMM', ))
 console.log(dateFns.format(present, 'YY', ))
 console.log(dateFns.format(present, 'dddd', ))
