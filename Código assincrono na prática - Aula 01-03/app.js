1050be78c4d6ccd1c67b33955f21565dc26100c0
Renar Freitas <renarfreitas@gmail.com>
2025-01-29 22:32:58 -0300

    Revisão Aula  01-02 Etapa 11
    Relembrando operação assíncrona.

--- Código assincrono na prática - Aula 01-03/app.js
+++ Código assincrono na prática - Aula 01-03/app.js
@@ -1,9 +1,19 @@
+/*
+O JS é por natureza uma lingagem Sincrona
+Ou seja executa linha por linha de cima para baixo...
+Uma linguagem Singol Thread! (Sequêncial de cima para baixo uma depois da outra)
+
+O que é uma operação assíncrona?
+É um código que inicia um processo 
+agora e finaliza esse processo posteriormente.
+*/
+
 console.log(1)
 console.log(2)
 
 setTimeout(() => {
   console.log('Função de callback executada!')
-}, 2000)
+}, 3000)
 
 console.log(3)
 console.log(4)
