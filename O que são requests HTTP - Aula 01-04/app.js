4b15a8fd8940c6d4e04bbd0d9dbe8fc3150d3db7
Renar Freitas <renarfreitas@gmail.com>
2025-02-01 21:46:01 -0300

    Revisão
    requests HTTP e mais.

--- O que são requests HTTP - Aula 01-04/app.js
+++ O que são requests HTTP - Aula 01-04/app.js
@@ -1,5 +1,25 @@
-// HTTP = Hypertext Transfer Protocol
+/* HTTP = Hypertext Transfer Protocol - Principal protocolo de comunicação entre servidores.
+Por que precisamos deles?
+Dentro de nossas aplicações, vamos precisar exibir informações de outro lugar
+E acessamos isso através de requisições HTTP, para nos conectarmos a uma API ou um BD
+Uma requisição é um pedido uma solicitação feita a um reservidor ou API
 
-// Endpoint = 
+requests HTPP
+response(informações)
 
-// JSON = JavaScript Object Notification
+// Endpoint = URL de uma API ou servidor, para que possamos acessar e obter as informações que o servidor ou API
+armazena
+
+// JSON = JavaScript Object Notification, padrão usado para troca de informações entre sistemas.
+
+acessando a url disponibilizado pelo JsonPlaceholder, obtemos o seguinte objeto:
+
+url:https://jsonplaceholder.typicode.com/todos/1
+{
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+}
+
+*/
